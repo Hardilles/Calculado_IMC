74698c5a9cba98ac8fa2e4cf6dd78a3b1dd3a6e9
Hardilles Germano <hardilles.germano@gmail.com>
2024-02-05 17:01:47 -0300

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
-function calcular(){
-   
+const form = document.getElementById('form');
 
-}
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+
+    const weigh = document.getElementById('weigt').value;
+    const heigt = document.getElementById('heigt').value;
+
+    const bmi = (weigh / (heigt * heigt)).toFixed(2); //toFixed = duas casas depois da virgula//
+
+    const value = document.getElementById('value');
+    let description = ''; //descrição do texto inicialmente vazia//
+
+    document.getElementById('infos').classList.remove('hidden')
+
+});
+
